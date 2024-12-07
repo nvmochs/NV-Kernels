6b17ee5f3ff1abaa4e75ad56576d12cca6a40837
Yi Liu <yi.l.liu@intel.com>
2024-12-07 04:01:08 -0800

    iommufd: Deal with IOMMU_HWPT_FAULT_ID_VALID in iommufd core
    IOMMU_HWPT_FAULT_ID_VALID is used to mark if the fault_id field of
    iommu_hwp_alloc is valid or not. As the fault_id field is handled in
    the iommufd core, so it makes sense to sanitize the
    IOMMU_HWPT_FAULT_ID_VALID flag in the iommufd core, and mask it out
    before passing the user flags to the iommu drivers.
    
    Link: https://patch.msgid.link/r/20241207120108.5640-1-yi.l.liu@intel.com
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    (cherry picked from commit 11534b4de2a1bcc438ed90d031184c9c847e8560)
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- drivers/iommu/iommufd/hw_pagetable.c
+++ drivers/iommu/iommufd/hw_pagetable.c
@@ -139,8 +139,8 @@ iommufd_hwpt_paging_alloc(struct iommufd_ctx *ictx, struct iommufd_ioas *ioas,
 	hwpt_paging->nest_parent = flags & IOMMU_HWPT_ALLOC_NEST_PARENT;
 
 	if (ops->domain_alloc_paging_flags) {
-		hwpt->domain = ops->domain_alloc_paging_flags(idev->dev, flags,
-							      user_data);
+		hwpt->domain = ops->domain_alloc_paging_flags(idev->dev,
+				flags & ~IOMMU_HWPT_FAULT_ID_VALID, user_data);
 		if (IS_ERR(hwpt->domain)) {
 			rc = PTR_ERR(hwpt->domain);
 			hwpt->domain = NULL;
@@ -279,6 +279,8 @@ iommufd_viommu_alloc_hwpt_nested(struct iommufd_viommu *viommu, u32 flags,
 	struct iommufd_hw_pagetable *hwpt;
 	int rc;
 
+	if (flags & ~IOMMU_HWPT_FAULT_ID_VALID)
+		return ERR_PTR(-EOPNOTSUPP);
 	if (!user_data->len)
 		return ERR_PTR(-EOPNOTSUPP);
 	if (!viommu->ops || !viommu->ops->alloc_domain_nested)
@@ -295,7 +297,9 @@ iommufd_viommu_alloc_hwpt_nested(struct iommufd_viommu *viommu, u32 flags,
 	hwpt_nested->parent = viommu->hwpt;
 
 	hwpt->domain =
-		viommu->ops->alloc_domain_nested(viommu, flags, user_data);
+		viommu->ops->alloc_domain_nested(viommu,
+				flags & ~IOMMU_HWPT_FAULT_ID_VALID,
+				user_data);
 	if (IS_ERR(hwpt->domain)) {
 		rc = PTR_ERR(hwpt->domain);
 		hwpt->domain = NULL;
